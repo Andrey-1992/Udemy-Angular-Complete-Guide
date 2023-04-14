8a8fd07be32fcdd85d9d36009e0cc1fa99a0ed17
Andrey Bermudez <andrey.bermudez92@gmail.com>
2023-04-14 11:25:45 -0600

    assign-function-for-event

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,6 +8,7 @@ import { Component } from '@angular/core';
 export class ServersComponent {
   allowNewServer = false;
   serverCreationStatus = 'No server was created';
+  serverName = '';
 
   constructor() {
     // setTimeout(() => {
@@ -17,6 +18,11 @@ export class ServersComponent {
 
   onCreateServer() {
     this.serverCreationStatus = 'Server was created';
+  }
+
+  onUpdateServerName(event: Event) {
+    // console.log(event);
+    // this.serverName = (<HTMLInputElementevent>event.target).value
   }
 
 }
